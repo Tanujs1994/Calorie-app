b561a0756401d361c5ef2ad7a6b2b391c694dc3a
Tanuj sharma <tanujsharma482@gmail.com>
2023-09-22 00:30:12 +0530

    go to pagedeatail page on card click

--- lib/dishitem.dart
+++ lib/dishitem.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/food_detail.dart';
 
 class MyDishItemCard extends StatelessWidget {
   final String imagePath;
@@ -9,25 +10,30 @@ class MyDishItemCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (isVegeterian == true) {
-     return Card(
-            child: ListTile(
-              leading: ClipRRect(borderRadius: BorderRadius.all(Radius.circular(10)),child: Image.network(imagePath, width:90,fit: BoxFit.cover,)),
-              title: Text(cardTitle),
-              subtitle: Row(
-                children: [
-                 Container(
-                  width: 10,
-                  height: 10,
-                  decoration: BoxDecoration(shape:BoxShape.circle,color: Colors.green,),
+     return InkWell(
+      onTap: (){
+        Navigator.push(context,MaterialPageRoute(builder:(context) => FoodDetailPage()) );
+      },
+       child: Card(
+              child: ListTile(
+                leading: ClipRRect(borderRadius: BorderRadius.all(Radius.circular(10)),child: Image.network(imagePath, width:90,fit: BoxFit.cover,)),
+                title: Text(cardTitle),
+                subtitle: Row(
+                  children: [
+                   Container(
+                    width: 10,
+                    height: 10,
+                    decoration: BoxDecoration(shape:BoxShape.circle,color: Colors.green,),
+                  ),
+                  Text(' VEG', style: TextStyle(color: Colors.green),),
+                  
+                  ],
+                 
                 ),
-                Text(' VEG', style: TextStyle(color: Colors.green),),
                 
-                ],
-               
               ),
-              
             ),
-          );
+     );
     }else{
       return Card(
             child: ListTile(
@@ -38,7 +44,7 @@ class MyDishItemCard extends StatelessWidget {
                 Container(
                   width: 10,
                   height: 10,
-                  decoration: BoxDecoration(shape:BoxShape.circle,color: Colors.green,),
+                  decoration: BoxDecoration(shape:BoxShape.circle,color: Colors.red,),
                 ),
                 Text(' NONVEG', style: TextStyle(color: Colors.red),),
                 ],
