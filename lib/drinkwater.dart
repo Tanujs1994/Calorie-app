5dee3f5ecbb173d2ee182757ce0f862dc0d4d382
Tanuj sharma <tanujsharma482@gmail.com>
2023-10-11 00:13:04 +0530

    make Glass widget

--- lib/drinkwater.dart
+++ lib/drinkwater.dart
@@ -1,53 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/glass.dart';
 
 class DrinkWater extends StatelessWidget {
+                                                         
   const DrinkWater({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 90),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 2,
-              width: 50,
-              color: Colors.black,
-            ),
-            Container(
-              height: 200,
-              width: 2,
-              color: Colors.black,
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
-                  height: 198,
-                  width: 1,
-                ),
-                Container(
-                  height: 2,
-                  width: 100,
-                  color: Colors.black,
-                ),
-              ],
-            ),
-            Container(
-              height: 200,
-              width: 2,
-              color: Colors.black,
-            ),
-            Container(
-              width: 50,
-              height: 2,
-              color: Colors.black,
-            )
-          ],
-        ),
-      ),
-    );
+    return Glass();
   }
 }
---
+++ lib/glass.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class Glass extends StatelessWidget {
+  const Glass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 90),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 2,
+              width: 50,
+              color: Colors.black,
+            ),
+            Container(
+              height: 200,
+              width: 2,
+              color: Colors.black,
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 198,
+                  width: 1,
+                ),
+                Container(
+                  height: 2,
+                  width: 100,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+            Container(
+              height: 200,
+              width: 2,
+              color: Colors.black,
+            ),
+            Container(
+              width: 50,
+              height: 2,
+              color: Colors.black,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
